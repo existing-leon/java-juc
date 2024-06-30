0388b46b9ec9c3f0613c93a250e90cd30bc2fa61
existing-leon <leon_hxk@163.com>
2024-07-01 00:17:10 +0800

    原理之Thread与Runnable的关系

--- demo/src/main/java/com/xiaokun/test/Test2.java
+++ demo/src/main/java/com/xiaokun/test/Test2.java
@@ -20,3 +20,15 @@ public class Test2 {
         t.start();
     }
 }
+
+/**
+ * 原理之Thread与Runnable的关系：
+ * 分析Thread的源码，理清它与Runnable的关系
+ * 小结：
+ *  * 方法1是把线程和任务合并在了一起，方法2是把线程和任务分开了
+ *  * 用Runnable更容易与线程池等高级API配合
+ *  * 用Runnable让任务脱离了Thread继承体系，更灵活
+ *
+ *
+ */
+

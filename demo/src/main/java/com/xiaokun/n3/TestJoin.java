74ab723c03c4079725a795c206c4f7fda3688cde
existing-leon <leon_hxk@163.com>
2024-07-03 00:03:22 +0800

    join的限时等待

--- demo/src/main/java/com/xiaokun/n3/TestJoin.java
+++ demo/src/main/java/com/xiaokun/n3/TestJoin.java
@@ -16,7 +16,31 @@ public class TestJoin {
     static int r2 = 0;
 
     public static void main(String[] args) throws InterruptedException {
-        test2();
+//        test2();
+        test3();
+    }
+
+    private static void test3() throws InterruptedException {
+        Thread t1 = new Thread(new Runnable() {
+            @SneakyThrows
+            @Override
+            public void run() {
+                sleep(2000);
+                r1 = 10;
+            }
+        });
+
+        long start = System.currentTimeMillis();
+        t1.start();
+
+        // 线程执行结束会导致join结束
+        log.debug("join begin...");
+        // 如果线程实际运行时间大于等待时间, 那么只会等待填入的等待时间
+        t1.join(1500);
+        // 如果线程实际运行时间小于等待时间, 那么只会等待线程实际运行的时间
+        // t1.join(3000);
+        long end = System.currentTimeMillis();
+        log.debug("r1: {}, r2: {}, cost: {}", r1, r2, end - start);
     }
 
     private static void test2() throws InterruptedException {
